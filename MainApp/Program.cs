ec83a48d6463886cef21e414a6d6898a8c05a5b5
ca20110820 <ca20110820@gmail.com>
2024-02-11 15:29:15 +0800

    feat: Created Example for Newtonsoft Json Deserialization with Nested Properties

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+//using System.Text.Json;
+
+
+async Task<List<Coin>> getAllCoins()
+{
+    using HttpClient client = new HttpClient();  // Instantiate a HTTP Client
+    try
+    {
+        HttpResponseMessage response = await client.GetAsync("https://api.coingecko.com/api/v3/coins/list?include_platform=true");
+
+        if (response.IsSuccessStatusCode)
+        {
+            string responseData = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<Coin>>(responseData);
+            //return JsonSerializer.Deserialize<List<Coin>>(responseData);
+        }
+        else
+        {
+            throw new Exception("No Response!");
+        }
+    }
+    catch (Exception err)
+    {
+        Console.WriteLine(err);
+        throw new Exception("No Response!");
+    }
+}
+
+
+async Task main()
+{
+    var coinList = await getAllCoins();
+    if (coinList != null)
+    {
+        foreach (Coin coin in coinList)
+        {
+            Console.WriteLine(coin);
+
+            foreach (var kvp in coin.Platforms)
+            {
+                Console.WriteLine($"{kvp.Key} : {kvp.Value}");
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("The Coin List is Empty!");
+    }
+}
+
+await main();
+
+class Coin
+{
+    public string ID { get; set; }
+    public string Name { get; set; }
+    public string Symbol { get; set; }
+
+    public Dictionary<string, string> Platforms { get; set; }
+
+    public override string ToString()
+    {
+        return $"ID={ID} | Name={Name} | Symbol={Symbol}";
+    }
+}
